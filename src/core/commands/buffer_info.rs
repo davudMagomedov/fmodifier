251ff61691f3c7bbb125cc15e50e4aadcdef384b
Davud Magomedov <davmagbek54321@gmail.com>
2024-05-24 23:57:17 +0400

    [ADDED] commands::buffer_info

---
+++ src/core/commands/buffer_info.rs
@@ -0,0 +1,32 @@
+use super::*;
+
+fn format_name(buffer_name: &str) -> String {
+    format!("Name: {}.", buffer_name)
+}
+
+fn format_size(len: usize) -> String {
+    format!("Size: {} bytes.", len)
+}
+
+/// The `present_info` function writes information about given buffer to given output.
+fn present_info(buffer: &Buffer, buffer_name: &str, output: &mut CoreOutput) {
+    output.push_info(format_name(buffer_name));
+    output.push_info(format_size(buffer.len()));
+}
+
+/// The `buffer_info` writes information about buffer with given name to output.
+///
+/// Output's format:
+/// - Name: <buffer_name>.
+/// - Size: <buffer_size>
+pub fn buffer_info(core: &Core, buffer_name: String) -> CoreResult<CoreOutput> {
+    let buffer = core
+        .variables
+        .get_buffer(&buffer_name)
+        .ok_or_else(|| CoreError::pass_new())?;
+
+    let mut output = CoreOutput::new();
+    present_info(buffer, &buffer_name, &mut output);
+
+    Ok(output)
+}
--- src/core/commands/mod.rs
+++ src/core/commands/mod.rs
@@ -5,6 +5,7 @@ use super::file::File;
 use super::core_e::{CoreError, CoreResult};
 use super::output::*;
 
+pub mod buffer_info;
 pub mod fill_buffer;
 pub mod make_buffer;
 pub mod show_buffer;
