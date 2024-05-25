df20a82b9a303c21e721832bb4dbbd5b162a1a7c
Davud Magomedov <davmagbek54321@gmail.com>
2024-05-25 23:58:00 +0400

    [ADDED] CoreError::{undefined_variable(), incorrect_index(), writing_to_read_only_file()}

--- src/core/core_e.rs
+++ src/core/core_e.rs
@@ -2,14 +2,43 @@ use std::error::Error as ErrorTrait;
 use std::fmt::{Display, Formatter, Result as FmtResult};
 use std::io::Error as IoError;
 
+#[derive(Debug, Clone)]
+enum CoreErrorEnum {
+    UndefinedVariable { variable_name: String },
+    IncorrectIndex { index: usize, top: usize },
+    WritingToReadOnlyFile { file_name: String },
+}
+
 pub type CoreResult<T> = Result<T, CoreError>;
 
 #[derive(Debug, Clone)]
-pub struct CoreError {}
+pub struct CoreError {
+    enumer: CoreErrorEnum,
+}
 
 impl CoreError {
     pub fn pass_new() -> Self {
-        CoreError {}
+        unimplemented!()
+    }
+}
+
+impl CoreError {
+    pub fn undefined_variable(variable_name: String) -> Self {
+        CoreError {
+            enumer: CoreErrorEnum::UndefinedVariable { variable_name },
+        }
+    }
+
+    pub fn incorrect_index(index: usize, top: usize) -> Self {
+        CoreError {
+            enumer: CoreErrorEnum::IncorrectIndex { index, top },
+        }
+    }
+
+    pub fn writing_to_read_only_file(file_name: String) -> Self {
+        CoreError {
+            enumer: CoreErrorEnum::WritingToReadOnlyFile { file_name },
+        }
     }
 }
 
