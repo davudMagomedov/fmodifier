602296121c5f0027238303697054a1866b4cd3e0
Davud Magomedov <davmagbek54321@gmail.com>
2024-05-27 22:08:26 +0400

    [REMOVED] command_handler module

--- src/command_handler/commands/core_cmd.rs
+++
@@ -1,8 +0,0 @@
-use crate::core::command::CoreCommand;
-use crate::core::core::Core;
-use crate::core::core_e::CoreResult;
-use crate::core::output::CoreOutput;
-
-pub fn execute_core_command(core: &mut Core, command: CoreCommand) -> CoreResult<CoreOutput> {
-    core.execute(command)
-}
--- src/command_handler/commands/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod buffer_write_bytes;
-pub mod core_cmd;
-
-pub use core_cmd::execute_core_command;
