93a7e2ffc278b44a2cc34399bdb0a8c93e02f244
Davud Magomedov <davmagbek54321@gmail.com>
2024-05-30 00:18:54 +0400

    [ADDED] Terminal: changed interface. Now the Terminal structure process cases when IO has failed

--- terminal/src/terminal.rs
+++ terminal/src/terminal.rs
@@ -2,14 +2,18 @@ use std::io::{stderr, stdin, stdout, Result as IoResult, Stderr, Stdin, Stdout, 
 
 const PROMPT: &str = "fmod> ";
 
+const IO_ERROR_MESSAGE: &str = "The I/O system has failed";
+
 /// The `Terminal` structure provides function for reading and writing to terminal. It provides
 /// such basic functionality as prompt to enter and so on.
+///
+/// The structure monitors I/O error and, if necessary, terminate the program.
 ///
 /// #### Example
 /// ```ignore
 /// let mut terminal = Terminal::new();
 ///
-/// let line = terminal.read_line()?;
+/// let line = terminal.read_line();
 ///
 /// let tokens = tokenize(line)?;
 /// let command = parse_tokens(&tokens)?;
@@ -33,7 +37,24 @@ impl Terminal {
 
     /// The `read_line` function returns string an user wrote to terminal. If the user didn't write
     /// anything, the function is blocked.
-    pub fn read_line(&mut self) -> IoResult<String> {
+    ///
+    /// When IO error happens, the function parses it automatically.
+    pub fn read_line(&mut self) -> String {
+        let io_result = self._read_line();
+        self.parse_io_result(io_result)
+    }
+
+    /// The `write` function obviously writes given string to terminal.
+    ///
+    /// When IO error happens, the function parses it automatically.
+    pub fn write(&mut self, string: &str) {
+        let io_result = self._write(string);
+        self.parse_io_result(io_result)
+    }
+
+    /// The `_read_line` function returns result of getting string from terminal. The function can
+    /// be blocked.
+    fn _read_line(&mut self) -> IoResult<String> {
         self.out_stream.write(PROMPT.as_bytes())?;
         self.out_stream.flush()?;
 
@@ -43,11 +64,18 @@ impl Terminal {
         Ok(buffer.trim().to_string())
     }
 
-    /// The `write` function obviously writes given string to terminal.
-    pub fn write(&mut self, string: &str) -> IoResult<()> {
+    /// The `_write` function returns result of writing string to terminal.
+    fn _write(&mut self, string: &str) -> IoResult<()> {
         self.out_stream.write(string.as_bytes())?;
         self.out_stream.flush()?;
 
         Ok(())
+    }
+
+    /// The `parse_io_result` takes IO result and returns value in Ok variant. If result is Err
+    /// variant, the function processes this case: maybe by program termination, maybe by warning,
+    /// etc.
+    fn parse_io_result<T>(&self, result: IoResult<T>) -> T {
+        result.expect(IO_ERROR_MESSAGE)
     }
 }
