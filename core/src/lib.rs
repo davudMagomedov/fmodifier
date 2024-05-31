0d9a8ddb750c7a6cc1b538b9fe606c7bbb2e56fb
Davud Magomedov <davmagbek54321@gmail.com>
2024-06-01 00:13:35 +0400

    [MODIFIED] convenient use

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,10 +1,17 @@
-pub mod buffer;
 pub mod command;
-pub mod commands;
 pub mod core;
 pub mod core_e;
-pub mod file;
 pub mod output;
 pub mod token;
 pub mod token_parser;
-pub mod variables;
+
+mod buffer;
+mod commands;
+mod file;
+mod variables;
+
+pub use command::*;
+pub use core::*;
+pub use core_e::*;
+pub use token::*;
+pub use token_parser::*;
