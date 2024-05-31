02b1b4090bacb9baafb86227ca4ef3c449002886
Davud Magomedov <davmagbek54321@gmail.com>
2024-06-01 00:34:00 +0400

    [ADDED

--- terminal/src/runner.rs
+++ terminal/src/runner.rs
@@ -1,5 +1,8 @@
+use crate::output::ToOutput;
 use crate::terminal::Terminal;
+use crate::tokenizer::tokenize;
 
+use core::parse_tokens;
 use core::Core;
 
 /// The `Runner` structure is iterator in which each iteration means following actions:
@@ -20,5 +23,42 @@ impl Runner {
             core: Core::new(),
             terminal: Terminal::new(),
         }
+    }
+
+    fn output<T: ToOutput>(&mut self, object: T) {
+        self.terminal.write(&format!("{}\n", object.to_output()));
+    }
+}
+
+impl Iterator for Runner {
+    type Item = ();
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let input = self.terminal.read_line();
+        let tokens = match tokenize(&input) {
+            Ok(tokens) => tokens,
+            Err(e) => {
+                self.output(e);
+                return Some(());
+            }
+        };
+        let command = match parse_tokens(&tokens) {
+            Ok(command) => command,
+            Err(e) => {
+                self.output(e);
+                return Some(());
+            }
+        };
+        let core_output = match self.core.execute(command) {
+            Ok(core_output) => core_output,
+            Err(e) => {
+                self.output(e);
+                return Some(());
+            }
+        };
+
+        self.output(core_output);
+
+        Some(())
     }
 }
