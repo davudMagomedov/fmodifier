dab334101a11e4765d88f1676c39942e3ca2a013
Davud Magomedov <davmagbek54321@gmail.com>
2024-05-31 20:42:40 +0400

    [ADDED] stringify_*(): beauty

--- terminal/src/output/core_output_impl.rs
+++ terminal/src/output/core_output_impl.rs
@@ -8,7 +8,7 @@ mod stringify_table_2_col;
 use stringify_table::stringify_table;
 use stringify_table_2_col::stringify_table_2_col;
 
-fn stringify_info_line(write_to: &mut String, info_line: &InfoLine) {
+fn stringify_info_line(info_line: &InfoLine, write_to: &mut String) {
     write_to.push_str("- ");
     write_to.push_str(info_line);
 }
@@ -18,11 +18,11 @@ fn stringify_info_line(write_to: &mut String, info_line: &InfoLine) {
 ///
 /// For this function is right used, push to string the `\n` character before calling or make sure
 /// that the string this function writes to is on empty line.
-fn stringify_infos(write_to: &mut String, infos: &[InfoLine]) {
+fn stringify_infos(infos: &[InfoLine], write_to: &mut String) {
     infos[..infos.len() - 1].into_iter().for_each(|info_line| {
-        stringify_info_line(write_to, info_line);
+        stringify_info_line(info_line, write_to);
         write_to.push('\n');
     });
 
-    stringify_info_line(write_to, &infos[infos.len() - 1]);
+    stringify_info_line(&infos[infos.len() - 1], write_to);
 }
