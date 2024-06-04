fd3c3a7f58eb943d7ab650f21b53d5ae61707109
Davud Magomedov <davmagbek54321@gmail.com>
2024-06-04 14:05:41 +0400

    [ADDED] StringRectangle::place_right_top()   [CHANGED] functionality StringRectangle::{place_right(), place_left()}

--- terminal/src/output/core_output_impl/stringify_table.rs
+++ terminal/src/output/core_output_impl/stringify_table.rs
@@ -113,15 +113,98 @@ impl StringRectangle {
         self.lines.push_front(inserting_line);
     }
 
-    pub fn place_right(self, other: StringRectangle) -> StringRectangle {
-        unimplemented!();
+    /// The `place_right_top` function takes a string rectangle and places it to the right of the
+    /// current one. If lengths of the string rectangle and the current one are different, the
+    /// string rectangle will be placed on top.
+    ///
+    /// If the given string rectangle is empty, the function just returns the same rectangle as it
+    /// was.
+    ///
+    /// #### Example
+    /// ```text
+    /// s1 is 4x5 rectangle = [
+    ///     [DAVUD]
+    ///     [LEON ]
+    ///     [HI   ]
+    ///     [BAN  ]
+    /// ]
+    /// s2 is 3x7 rectangle = [
+    ///     [HELLO  ]
+    ///     [CONV   ]
+    ///     [FMODIF ]
+    /// ]
+    /// s1.place_right_top(s2) is 4x12 rectangle = [
+    ///     [DAVUDHELLO  ]
+    ///     [LEON CONV   ]
+    ///     [HI   FMODIF ]
+    ///     [BAN         ]
+    /// ]
+    /// ```
+    pub fn place_right_top(self, other: StringRectangle) -> StringRectangle {
+        if self.length() >= other.length() {
+            // Then, `self.length()` is greater than `other.length()` on `larger`.
+            let empty_rectangle_length = self.length() - other.length();
+            let empty_rectangle_width = other.width();
+
+            let empty_rectangle =
+                StringRectangle::fill(empty_rectangle_width, empty_rectangle_length, EMPTY_CHAR);
+
+            self.place_right(
+                // SAFETY: `other.width == empty_rectangle.width`.
+                unsafe { other.place_bottom_unchecked(empty_rectangle) },
+            )
+        } else {
+            // Then, `self.length()` is less than `other.length()` on `larger`.
+            let empty_rectangle_length = other.length() - self.length();
+            let empty_rectangle_width = self.width();
+
+            let empty_rectangle =
+                StringRectangle::fill(empty_rectangle_width, empty_rectangle_length, EMPTY_CHAR);
+
+            (
+                // SAFETY: `self.width == empty_rectangle.width`.
+                unsafe { self.place_bottom_unchecked(empty_rectangle) }
+            )
+            .place_right(other)
+        }
     }
 
-    /// The `place_left` takes other string rectangle and places it in left of the current one.
-    ///
-    /// Count of lines of other string rectangle must be the same as in the current one.
+    /// The `place_right` function places a string rectangle in right of the current one. If number
+    /// of the lengths of two rectangles are different, the function cuts one of two to another
+    /// one.
+    fn place_right(self, other: StringRectangle) -> StringRectangle {
+        let lines = self
+            .lines
+            .into_iter()
+            .zip(other.lines.into_iter())
+            .map(|(self_line, other_line)| self_line + &other_line)
+            .collect::<Vec<_>>();
+
+        StringRectangle::new_with_lines(
+            &lines
+                .iter()
+                .map(|string| string.as_str())
+                .collect::<Vec<_>>(),
+        )
+    }
+
+    /// The `place_left` function places given string rectangle in the left of the current one. If
+    /// lengths of the two rectangles are different, the function cuts one of two to length of
+    /// another one.
     pub fn place_left(self, other: StringRectangle) -> StringRectangle {
-        unimplemented!();
+        let lines = self
+            .lines
+            .into_iter()
+            .zip(other.lines.into_iter())
+            .map(|(self_line, other_line)| other_line + &self_line)
+            .collect::<Vec<_>>();
+
+        StringRectangle::new_with_lines(
+            &lines
+                .iter()
+                .map(|string| string.as_str())
+                .collect::<Vec<_>>(),
+        )
     }
 
     /// The `place_bottom` function takes other string rectangle and places it in the bottom of the
