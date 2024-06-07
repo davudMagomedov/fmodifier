cf8dbf9d11560bc5b51e0e131c91ad7e0685400e
Davud Magomedov <davmagbek54321@gmail.com>
2024-06-07 14:35:02 +0400

    [MODIFIED] build_app(): msth

--- fmodifier/src/clap_app.rs
+++ fmodifier/src/clap_app.rs
@@ -1,15 +1,19 @@
 use clap::{crate_authors, crate_name, crate_version, Arg, Command};
 
 pub const FILE_NAME_ARG: &str = "file_name";
+pub const EXEC_FILE_SUBCMD: &str = "execfile";
 
 /// The `build_app` function returns specially built `clap::Command` object.
 pub fn build_app() -> Command {
     Command::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
-        .arg(
-            Arg::new(FILE_NAME_ARG)
-                .required(false)
-                .help("For executing a file"),
+        .subcommand_required(false)
+        .subcommand(
+            Command::new(EXEC_FILE_SUBCMD).arg(
+                Arg::new(FILE_NAME_ARG)
+                    .required(false)
+                    .help("For executing a file"),
+            ),
         )
 }
