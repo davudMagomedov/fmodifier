6d5998af0a54b6e1bfab4653974f0c2fbc872034
Davud Magomedov <davmagbek54321@gmail.com>
2024-06-07 12:42:03 +0400

    [MODIFIED] 'file_name' placed into its own constant

--- fmodifier/src/clap_app.rs
+++ fmodifier/src/clap_app.rs
@@ -1,12 +1,14 @@
 use clap::{crate_authors, crate_name, crate_version, Arg, Command};
 
+const FILE_NAME_ARG: &str = "file_name";
+
 /// The `build_app` function returns specially built `clap::Command` object.
 pub fn build_app() -> Command {
     Command::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .arg(
-            Arg::new("executable_file_name")
+            Arg::new(FILE_NAME_ARG)
                 .required(false)
                 .help("For executing a file"),
         )
