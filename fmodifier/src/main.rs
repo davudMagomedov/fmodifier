5a322dda7b7590171062fdf9fc65195d6b5cfff8
Davud Magomedov <davmagbek54321@gmail.com>
2024-06-07 14:58:10 +0400

    [DELETED] some shits

--- fmodifier/src/main.rs
+++ fmodifier/src/main.rs
@@ -2,9 +2,6 @@ mod app;
 mod clap_app;
 mod run;
 
-use terminal::commander::Terminal;
-use terminal::runner::Runner;
-
 fn main() {
     let application = app::App::new();
     run::run(&application);
--- fmodifier/src/run.rs
+++ fmodifier/src/run.rs
@@ -1,5 +1,3 @@
-#![allow(dead_code)]
-
 use crate::app::{App, AppAction};
 
 use terminal::commander::{FileReader, Terminal};
