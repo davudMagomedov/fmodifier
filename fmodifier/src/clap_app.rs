59eab2514317bdf7035ed0abf79bb900fb088445
Davud Magomedov <davmagbek54321@gmail.com>
2024-06-07 12:43:32 +0400

    [MODIFIED] constant made public

--- fmodifier/src/clap_app.rs
+++ fmodifier/src/clap_app.rs
@@ -1,6 +1,6 @@
 use clap::{crate_authors, crate_name, crate_version, Arg, Command};
 
-const FILE_NAME_ARG: &str = "file_name";
+pub const FILE_NAME_ARG: &str = "file_name";
 
 /// The `build_app` function returns specially built `clap::Command` object.
 pub fn build_app() -> Command {
