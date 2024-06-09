aebc2f45e6706bbdda1c65c8c7b866e790d8ac3e
Davud Magomedov <davmagbek54321@gmail.com>
2024-06-09 10:33:20 +0400

    [ADDED] One more semantic condition for those who implements Commander

--- src/terminal/commander/mod.rs
+++ src/terminal/commander/mod.rs
@@ -6,6 +6,9 @@ pub use terminal::Terminal;
 
 /// The `Commander` trait issues commands an accepts results. What to do with the results and how
 /// to issue commands is determined by the implementation.
+///
+/// Also a structure that implements `Commander` trait filters comments: if a comment were written,
+/// the `read_command` function has to be replaced it to empty string.
 pub trait Commander {
     fn is_terminal() -> bool;
     fn read_command(&mut self) -> Option<String>;
