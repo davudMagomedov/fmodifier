938eec00c5bf16697249ac22bf9be4affe60f254
Davud Magomedov <davmagbek54321@gmail.com>
2024-06-23 17:02:35 +0400

    [MODIFIED] Added inserting variables in running

--- src/bin/fmodifier/run.rs
+++ src/bin/fmodifier/run.rs
@@ -1,20 +1,42 @@
-use crate::app::{App, AppAction};
+use crate::app::{App, AppAction, Pair};
 
-use fmodifier::terminal::commander::{FileReader, Terminal};
+use fmodifier::core::CoreCommand;
+use fmodifier::terminal::commander::{Commander, FileReader, Terminal};
 use fmodifier::terminal::runner::Runner;
 
 const FILE_UNEXISTS_ERROR: &str = "A file with the name isn't found.";
 
-fn run_interactive() {
+fn insert_variables_to_runner<C: Commander>(app: &App, runner: &mut Runner<C>) {
+    app.variables().into_iter().for_each(|pair| match pair {
+        Pair::IntegerVar { name, value } => {
+            let _ = runner.execute(CoreCommand::SetVariableInteger {
+                variable_name: name,
+                value,
+            });
+        }
+        Pair::StringVar { name, value } => {
+            let _ = runner.execute(CoreCommand::SetVariableString {
+                variable_name: name,
+                value,
+            });
+        }
+    });
+}
+
+fn run_interactive(app: &App) {
     let terminal = Terminal::new();
-    let runner = Runner::new(terminal);
+    let mut runner = Runner::new(terminal);
+
+    insert_variables_to_runner(app, &mut runner);
 
     runner.for_each(|_| {});
 }
 
-fn run_from_file(file_path: &str) {
+fn run_from_file(app: &App, file_path: &str) {
     let file_reader = FileReader::new(file_path).expect(FILE_UNEXISTS_ERROR);
-    let runner = Runner::new(file_reader);
+    let mut runner = Runner::new(file_reader);
+
+    insert_variables_to_runner(app, &mut runner);
 
     runner.for_each(|_| {});
 }
@@ -23,8 +45,8 @@ fn run_from_file(file_path: &str) {
 pub fn run(app: &App) {
     match app.action() {
         AppAction::RunInteractive => {
-            run_interactive();
+            run_interactive(app);
         }
-        AppAction::RunFromFile { file_name } => run_from_file(&file_name),
+        AppAction::RunFromFile { file_name } => run_from_file(app, &file_name),
     }
 }
