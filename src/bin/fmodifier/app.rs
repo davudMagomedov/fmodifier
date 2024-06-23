0ac74229e87637867c42b18e51afac3e75eec125
Davud Magomedov <davmagbek54321@gmail.com>
2024-06-23 16:48:12 +0400

    [ADDED] variables in cli

--- src/bin/fmodifier/app.rs
+++ src/bin/fmodifier/app.rs
@@ -4,14 +4,55 @@ use crate::clap_app::{build_app, *};
 
 use clap::ArgMatches;
 
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum AppAction {
     RunInteractive,
     RunFromFile { file_name: String },
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Pair {
+    StringVar { name: String, value: String },
+    IntegerVar { name: String, value: usize },
+}
+
 /// The `App` structure is responsible for parsing CLI arguments.
 pub struct App {
     matches: ArgMatches,
+}
+
+fn is_right_name(string: &str) -> bool {
+    let mut chars = string.chars();
+
+    chars
+        .next()
+        .map(|ch| matches!(ch, 'a'..='z' | 'A'..='Z' | '_'))
+        .unwrap_or(false)
+        && chars.all(|ch| matches!(ch, 'a'..='z' | 'A'..='Z' | '_' | '0'..='9'))
+}
+
+fn parse_assignment_string(string: &str) -> Option<Pair> {
+    let equal_pos = string.find('=')?;
+
+    let name = string[..equal_pos].trim();
+    let right_value = string[equal_pos + 1..].trim();
+
+    match right_value.parse::<usize>() {
+        Ok(value) => Some(Pair::IntegerVar {
+            name: name.to_string(),
+            value,
+        }),
+        Err(_) => {
+            if is_right_name(right_value) {
+                Some(Pair::StringVar {
+                    name: name.to_string(),
+                    value: right_value.to_string(),
+                })
+            } else {
+                return None;
+            }
+        }
+    }
 }
 
 impl App {
@@ -34,6 +75,33 @@ impl App {
             None => AppAction::RunInteractive,
             _ => unreachable!(),
         }
+    }
+
+    /// The `variables` function returns all variables that were defined in CLI.
+    pub fn variables(&self) -> Vec<Pair> {
+        let mut unparsed = Vec::new();
+
+        let pairs: Vec<Pair> = self
+            .matches
+            .get_many::<String>(VARIABLE_ARG)
+            .unwrap_or_default()
+            .filter_map(|string| match parse_assignment_string(string) {
+                Some(pair) => Some(pair),
+                None => {
+                    unparsed.push(string);
+                    None
+                }
+            })
+            .collect();
+
+        if !unparsed.is_empty() {
+            println!("The program couldn't parse the following assignments:");
+            unparsed.iter().for_each(|&string| {
+                println!("  {}", string);
+            });
+        }
+
+        pairs
     }
 
     fn new_with_matches(matches: ArgMatches) -> App {
--- src/bin/fmodifier/clap_app.rs
+++ src/bin/fmodifier/clap_app.rs
@@ -1,18 +1,27 @@
-use clap::{crate_authors, crate_name, crate_version, Arg, Command};
+use clap::{crate_authors, crate_description, crate_name, crate_version, Arg, ArgAction, Command};
 
 pub const FILE_NAME_ARG: &str = "file_name";
 pub const EXEC_FILE_SUBCMD: &str = "execfile";
+pub const VARIABLE_ARG: &str = "variable";
 
 /// The `build_app` function returns specially built `clap::Command` object.
 pub fn build_app() -> Command {
     Command::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
+        .about(crate_description!())
         .subcommand_required(false)
+        .arg(
+            Arg::new(VARIABLE_ARG)
+                .required(false)
+                .long(VARIABLE_ARG)
+                .short('v')
+                .action(ArgAction::Append),
+        )
         .subcommand(
             Command::new(EXEC_FILE_SUBCMD).arg(
                 Arg::new(FILE_NAME_ARG)
-                    .required(false)
+                    .required(true)
                     .help("For executing a file"),
             ),
         )
